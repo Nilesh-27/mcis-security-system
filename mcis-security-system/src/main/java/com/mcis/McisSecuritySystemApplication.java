d2ad97832ed4d46325e7f889bb331ef1e883336a
Dokuparthi Nilesh <dnileshmohan@gmail.com>
2026-07-17 16:55:24 +0200

    Initial Spring Boot Setup

---
+++ mcis-security-system/src/main/java/com/mcis/McisSecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package com.mcis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McisSecuritySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McisSecuritySystemApplication.class, args);
+	}
+
+}
---
+++ mcis-security-system/src/test/java/com/mcis/McisSecuritySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McisSecuritySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
